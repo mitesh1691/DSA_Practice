4c978c57f097839df4acbfd4e4976f0a396359ca
Mitesh Shinde <93754832+mitesh1691@users.noreply.github.com>
2023-08-07 19:18:37 +0530

    adding files

---
+++ accenture/solution.java
@@ -0,0 +1,37 @@
+package accenture;
+
+import java.util.Scanner;
+
+public class solution {
+    public static int ratcount(int r, int unit, int n, int[] arr){
+        if(arr == null){
+            return -1;
+        }
+        int totalFood = r * unit;
+        int sum = 0;
+        int count = 0;
+        for(int i = 0; i<n; i++){
+            sum = sum + arr[i];
+            count++;
+            if(sum >= totalFood){
+                break;
+            }
+        }
+        if(sum < totalFood){
+            return 0;
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int unit = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(ratcount(r, unit, n, arr));
+    }
+}
